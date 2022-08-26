ef416436477c8e1928cff3f0b64f8435fd95a6ee
unknown <chandan.mandre@doceazy.com>
2022-08-26 12:27:41 +0530

    bug fix

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const connectDB = require('./config/connection');
 app.use(express.json())
 app.use(cors({origin : "*"}));
 app.use(express.urlencoded({extended:true}))
+const PORT = process.env.PORT || 3000;
 connectDB();
 
 
 app.use('/client',require('./routes/clientRoutes'))
 app.use('/admin',require('./routes/adminRoutes'))
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log(`app is listing on ${process.env.APP_BASE_URL}`)
 });
